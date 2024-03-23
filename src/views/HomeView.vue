69a41a9a86cfb63d6b4343adcb18de13546add52
Luis Pardo <luispardosuarez@gmail.com>
2024-03-23 20:40:18 +0100

    fix issues hv

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,43 @@
 <script setup>
 
+  import AuthService from '@/services/AuthService';
+  import { useRouter } from 'vue-router';
+
+  const credentials = {
+    username: '',
+    password: '',
+  };
+
+  const router = useRouter();
+
+  const login = async () => {
+    try {
+      const response = await AuthService.login(credentials);
+      router.push('/solicitudes');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 </script>
 
-<template>
-  <main>
 
+<template>
+
+  <main>
+    <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="credentials.username" placeholder="Username" />
+      <input type="password" v-model="credentials.password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
   </main>
+
 </template>
 
-<style >
+<style scoped lang="scss">
+
 
 </style>
