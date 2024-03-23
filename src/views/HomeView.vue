9bfee280c16cba3c3906fe9f4e317075cbd4f424
Luis Pardo <luispardosuarez@gmail.com>
2024-03-23 19:08:27 +0100

    Implemented login functionality with username and password inputs.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,22 @@
 <script setup>
+  
 
+  
 </script>
 
 <template>
-  <main>
 
+  <main>
+    <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="credentials.username" placeholder="Username" />
+      <input type="password" v-model="credentials.password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
   </main>
+
 </template>
 
 <style >
