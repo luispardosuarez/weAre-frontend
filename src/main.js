319e4af7df0cda30648b0f1d4fee91fe11ef81a7
Luis Pardo <luispardosuarez@gmail.com>
2024-03-24 02:55:15 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import './assets/scss/main.scss'
 
-createApp(App).use(router).mount('#app');
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
