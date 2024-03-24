0b52d9a4fd676062e369f886c77ec1ebdcea6f81
Luis Pardo <luispardosuarez@gmail.com>
2024-03-24 20:47:19 +0100

    made some app.vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,70 @@
-<script>
+<script setup>
+
+import AuthService from './services/AuthService';
+
+const nombreUsuario = localStorage.getItem('user');
+
+const logout = () => {
+  AuthService.logout();
+};
 
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar">
+    <router-link to="/" class="navbar-logo">
+      <img src="../src/assets/img/logo_weAre-removebg-preview.png" alt="Logo Empresa"> 
+    </router-link>
+
+    <div class="navbar-user">
+      <span>WeAre, {{ nombreUsuario }}</span>
+      <button @click="logout">
+        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" fill="white" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" alt="Logout"/>
+  </svg>
+      </button>
+    </div>
   </nav>
-  <router-view/>
 </template>
 
 <style scoped lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+}
 
+.gradient-custom-2 {
+  background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+  color: white; /* Color del texto blanco */
+}
 
-  nav {
-    padding: 30px;
-  }
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
 
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+.navbar-logo img {
+  height: 60px; 
+}
 
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
+.navbar-user {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-user span {
+  margin-right: 10px; /* Ajustamos el margen derecho del texto */
+}
+
+.navbar-user button {
+  background: none;
+  border: none;
+  cursor: pointer;
 }
 
 </style>
